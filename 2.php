61d016ab87adf8886cf98febd71f07d8ead7681d
Love Dixit <146636723+lovekumardixit@users.noreply.github.com>
2024-02-23 13:31:42 +0530

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,13 @@
+<?php
+$choice=1;
+if ($choice==1){
+    echo"red light :stop";
+}
+else if ($choice==2){
+    echo "green light :go";
+}
+else if ($choice==3){
+    echo "yellow ligth :show down";
+
+}
+?>
---
+++ 6.php
@@ -0,0 +1,10 @@
+<?php
+$username="lovedixit";
+$password="Love@8954";
+if ($username=="Lovedixit" and $password=="Love@8954"){
+    echo "LOGIN SUCCESSFUL";
+}
+else {
+    echo "INVALID CREDENTIALS";
+}
+?>
